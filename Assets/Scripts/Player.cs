eab962af9bd9c1f74ab680deeeaa649c1b74b28a
MENUET Erwan Ext OBS OAB <emenuet.ext@orange.com>
2019-01-03 14:33:18 +0100

    Project creation

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 4f;
+    public float jumpHeight = 20f;
+    public float gravity = 50f;
+
+    Transform transform;
+    Rigidbody rigidbody;
+
+    public void Start()
+    {
+        transform = GetComponent<Transform>();
+
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.freezeRotation = true;
+    }
+
+    public void move(Vector3 translationVector)
+    {
+        this.transform.Translate(translationVector);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Player))]
+public class PlayerController : MonoBehaviour
+{
+    Player player;
+
+    void Start()
+    {
+        player = GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = new Vector3();
+        if (Input.GetKey(KeyCode.Z))
+        {
+            direction = Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction = -Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction = Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            direction = -Vector3.right;
+        }
+
+        player.move(direction);
+    }
+}
